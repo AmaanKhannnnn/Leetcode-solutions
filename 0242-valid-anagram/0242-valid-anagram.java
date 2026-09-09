dc5ae33ab9dd10039d347a42474f47bd24714a99
AmaanKhannnnn <amaankhanofficial0309@gmail.com>
2026-09-09 22:49:09 +0530

    Time: 7 ms (32.33%), Space: 44.1 MB (89.45%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int arr[] = new int[26];
+        if(s.length() != t.length()){
+            return false;
+        }
+        for(int i =0; i< s.length(); i++){
+            int k = s.charAt(i) - 'a';
+            arr[k]++;
+        }
+        for(int i =0; i<t.length(); i++){
+            int k = t.charAt(i) - 'a';
+            arr[k]--;
+        }
+        for(int i=0; i<26; i++){
+            if(arr[i] !=0){
+                return false;
+            }
+        } return true;
+
+    }
+}
